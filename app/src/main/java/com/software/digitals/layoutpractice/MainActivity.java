7c3637b1e83ca044d06d4038c305d6ace3614d34
mitcho85 <mitcho85@gmail.com>
2017-12-17 16:15:43 -0800

    Added exaggerated drop shadow using elevation.

--- app/src/main/java/com/software/digitals/layoutpractice/MainActivity.java
+++ app/src/main/java/com/software/digitals/layoutpractice/MainActivity.java
@@ -1,13 +1,37 @@
 package com.software.digitals.layoutpractice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageView mImageView = findViewById(R.id.mitchCircle);
+
+        final AlphaAnimation alphaAnimation = new AlphaAnimation(0F, 1F);
+        alphaAnimation.setDuration(1000);
+
+        setupAnimation(mImageView, alphaAnimation);
+    }
+
+    private void setupAnimation(ImageView view, final Animation animation) {
+        view.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // If the button is checked, load the animation from the given resource
+                // id instead of using the passed-in animation paramter. See the xml files
+                // for the details on those animations.
+                v.startAnimation(animation);
+            }
+        });
     }
 }
